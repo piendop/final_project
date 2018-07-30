32016a73478aaad0cfa03a55f07cbb333f6fd660
loctran1902apcs <ttloc@apcs.vn>
2018-07-30 21:04:02 +0700

    Add signUp + Update new UI

--- Miniproject01/app/src/main/java/searchlocation/miniproject01/UI/Login/LoginActivity.java
+++ Miniproject01/app/src/main/java/searchlocation/miniproject01/UI/Login/LoginActivity.java
@@ -29,6 +29,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -92,7 +93,16 @@ public class LoginActivity extends AppCompatActivity {
 				attemptLogin();
 			}
 		});
-		mProgressView = findViewById(R.id.login_progress);
+
+		Button mSignUpButton = (Button) findViewById(R.id.sign_up_link);
+		mSignUpButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent signUpActivity = new Intent(LoginActivity.this,SignUp.class);
+				startActivity(signUpActivity);
+			}
+		});
+
 	}
 
 	private void populateAutoComplete() {
@@ -202,97 +212,6 @@ public class LoginActivity extends AppCompatActivity {
 	}
 
 	/**
-	 * Shows the progress UI and hides the login form.
-	 */
-	/*
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-	private void showProgress(final boolean show) {
-		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-		// for very easy animations. If available, use these APIs to fade-in
-		// the progress spinner.
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-			int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-			mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-					show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-				@Override
-				public void onAnimationEnd(Animator animation) {
-					mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-				}
-			});
-
-			mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-			mProgressView.animate().setDuration(shortAnimTime).alpha(
-					show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-				@Override
-				public void onAnimationEnd(Animator animation) {
-					mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-				}
-			});
-		} else {
-			// The ViewPropertyAnimator APIs are not available, so simply show
-			// and hide the relevant UI components.
-			mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-		//	mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-		}
-	}
-
-	@Override
-	public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-		return new CursorLoader(this,
-				// Retrieve data rows for the device user's 'profile' contact.
-				Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-						ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-				// Select only email addresses.
-				ContactsContract.Contacts.Data.MIMETYPE +
-						" = ?", new String[]{ContactsContract.CommonDataKinds.Email
-				.CONTENT_ITEM_TYPE},
-
-				// Show primary email addresses first. Note that there won't be
-				// a primary email address if the user hasn't specified one.
-				ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-	}
-
-	@Override
-	public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-		List<String> emails = new ArrayList<>();
-		cursor.moveToFirst();
-		while (!cursor.isAfterLast()) {
-			emails.add(cursor.getString(ProfileQuery.ADDRESS));
-			cursor.moveToNext();
-		}
-
-		addEmailsToAutoComplete(emails);
-	}
-
-	@Override
-	public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-	}
-
-	private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-		//Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-		ArrayAdapter<String> adapter =
-				new ArrayAdapter<>(LoginActivity.this,
-						android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-		mEmailView.setAdapter(adapter);
-	}
-
-
-	private interface ProfileQuery {
-		String[] PROJECTION = {
-				ContactsContract.CommonDataKinds.Email.ADDRESS,
-				ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-		};
-
-		int ADDRESS = 0;
-		int IS_PRIMARY = 1;
-	}
-*/
-	/**
 	 * Represents an asynchronous login/registration task used to authenticate
 	 * the user.
 	 */
@@ -322,9 +241,11 @@ public class LoginActivity extends AppCompatActivity {
 				if (pieces[0].equals(mEmail)) {
 					// Account exists, return true if the password matches.
 					return pieces[1].equals(mPassword);
+				} else {
+					// If account does not exist then trigger fragment LoginToSignUp
+					Toast.makeText(LoginActivity.this, "Account does not exist.", Toast.LENGTH_SHORT).show();
 				}
 			}
-
 			// TODO: register the new account here.
 			return true;
 		}
@@ -359,6 +280,5 @@ public class LoginActivity extends AppCompatActivity {
 		Intent loadIntro = new Intent(LoginActivity.this,IntroActivity.class);
 		startActivity(loadIntro);
 	}
-
 }
 
---
+++ Miniproject01/app/src/main/java/searchlocation/miniproject01/UI/Login/SignUp.java
@@ -0,0 +1,148 @@
+package searchlocation.miniproject01.UI.Login;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import searchlocation.miniproject01.R;
+
+public class SignUp extends AppCompatActivity {
+
+	View _signupButton = findViewById(R.id.sign_up_button);
+	EditText _nameText = findViewById(R.id.input_name);
+	EditText _emailText = findViewById(R.id.input_email);
+	EditText _passwordText = findViewById(R.id.input_password);
+	EditText _reEnterPasswordText = findViewById(R.id.input_Repassword);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sign_up);
+
+
+		Button mLoginButton = (Button) findViewById(R.id.login_link);
+		mLoginButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent loginActivity = new Intent(SignUp.this,LoginActivity.class);
+				startActivity(loginActivity);
+			}
+		});
+
+		Button mSignUpButton = (Button) findViewById(R.id.sign_up_button);
+		mSignUpButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				attempSignUp();
+			}
+		});
+	}
+	public void attempSignUp(){
+		if (!validate()) {
+			onSignupFailed();
+			return;
+		}
+		_signupButton.setEnabled(false);
+/*
+		final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
+				R.style.AppTheme_Dark_Dialog);
+		progressDialog.setIndeterminate(true);
+		progressDialog.setMessage("Creating Account...");
+		progressDialog.show();*/
+
+		String name = _nameText.getText().toString();
+		String email = _emailText.getText().toString();
+		String password = _passwordText.getText().toString();
+		String reEnterPassword = _reEnterPasswordText.getText().toString();
+
+		// TODO: Implement your own signup logic here.
+
+		new android.os.Handler().postDelayed(
+				new Runnable() {
+					public void run() {
+						// On complete call either onSignupSuccess or onSignupFailed
+						// depending on success
+						onSignupSuccess();
+						// onSignupFailed();
+					//	progressDialog.dismiss();
+					}
+				}, 3000);
+	}
+
+	public void onSignupSuccess() {
+		_signupButton.setEnabled(true);
+		setResult(RESULT_OK, null);
+		loadIntroActivity();
+	}
+
+	public void onSignupFailed() {
+		Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+		_signupButton.setEnabled(true);
+	}
+	public boolean validate() {
+		boolean valid = true;
+
+		String name = _nameText.getText().toString();
+		String address = _addressText.getText().toString();
+		String email = _emailText.getText().toString();
+		String mobile = _mobileText.getText().toString();
+		String password = _passwordText.getText().toString();
+		String reEnterPassword = _reEnterPasswordText.getText().toString();
+
+		if (name.isEmpty() || name.length() < 3) {
+			_nameText.setError("at least 3 characters");
+			valid = false;
+		} else {
+			_nameText.setError(null);
+		}
+
+		if (address.isEmpty()) {
+			_addressText.setError("Enter Valid Address");
+			valid = false;
+		} else {
+			_addressText.setError(null);
+		}
+
+
+		if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+			_emailText.setError("enter a valid email address");
+			valid = false;
+		} else {
+			_emailText.setError(null);
+		}
+
+		if (mobile.isEmpty() || mobile.length()!=10) {
+			_mobileText.setError("Enter Valid Mobile Number");
+			valid = false;
+		} else {
+			_mobileText.setError(null);
+		}
+
+		if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+			_passwordText.setError("between 4 and 10 alphanumeric characters");
+			valid = false;
+		} else {
+			_passwordText.setError(null);
+		}
+
+		if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
+			_reEnterPasswordText.setError("Password Do not match");
+			valid = false;
+		} else {
+			_reEnterPasswordText.setError(null);
+		}
+
+		return valid;
+	}
+	private void loadIntroActivity() {
+		Intent loadIntro = new Intent(SignUp.this,IntroActivity.class);
+		startActivity(loadIntro);
+	}
+
+}
