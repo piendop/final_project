eadd86547dc91f9b21c62b1767eb2b66eded0f5f
Pien Dop <dtbao@apcs.vn>
2018-08-18 19:13:02 +0700

    update discover activity

--- Miniproject01/app/src/main/java/searchlocation/miniproject01/UI/Discover/DiscoverActivity.java
+++ Miniproject01/app/src/main/java/searchlocation/miniproject01/UI/Discover/DiscoverActivity.java
@@ -21,15 +21,12 @@ import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-import com.parse.ParseUser;
-import searchlocation.miniproject01.UI.ParseApplication;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import searchlocation.miniproject01.Models.Plan;
 import searchlocation.miniproject01.R;
-import searchlocation.miniproject01.UI.Login.LoginActivity;
 import searchlocation.miniproject01.UI.Utilis.Adapter;
 import searchlocation.miniproject01.UI.Utilis.BottomNavigationViewHelper;
 
@@ -41,63 +38,16 @@ public class DiscoverActivity extends AppCompatActivity {
 	private Adapter mAdapter;
     ArrayList<Plan> planList = new ArrayList<>();
 
-	String[][] data = {
-			{"Du lịch Phú Quốc: Cẩm nang từ A đến Z","#phuquoc#3ngay2dem#","Phú Quốc là quần đảo xinh đẹp nằm sâu trong vùng vịnh Thái Lan, thuộc tỉnh Kiên Giang. Ở vùng biển phía Nam của tổ quốc, đảo Ngọc Phú Quốc – hòn đảo lớn nhất của Việt Nam…"},
-			{"Du lịch Phú Quốc: Cẩm nang từ A đến Z","#phuquoc#3ngay2dem#","Mì Quảng, bún mắm nêm, gỏi mít, ốc hút, là những món đặc trưng nhất định phải thử khi đến Đà Nẵng."},
-			{"‘Ăn hết’ món ngon đường phố Đà Nẵng với 200k","#danang#2ngay3dem#duongpho#amthuc","Mì Quảng, bún mắm nêm, gỏi mít, ốc hút, là những món đặc trưng nhất định phải thử khi đến Đà Nẵng."}
-	};
-	int[] imgId = {R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_discover);
-		//new MultiplyTask().execute();
 		listOfPlans = (ListView) findViewById(R.id.list_plans);
-        planList.clear();
-        ParseQuery<ParseObject> query = new ParseQuery<>("Plan");
-        SharedPreferences sharedPreferences = this.getSharedPreferences("SharedPref",MODE_PRIVATE);
-        String username = sharedPreferences.getString("USERNAME",null);
-        query.whereEqualTo("userId", username);
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(final List<ParseObject> objects, ParseException e) {
-                if(e==null && objects.size()>0){
-                    for(final ParseObject object:objects){
-                        final Plan plan = new Plan();
-                        //load image to view
-                        ParseFile image = (ParseFile) object.get("image");
-                        if(image!=null){
-                            image.getDataInBackground(new GetDataCallback() {
-                                @Override
-                                public void done(byte[] data, ParseException e) {
-                                    if(e==null&&data!=null){
-                                        Log.i("Get image","successful");
-                                        Bitmap bitmap = BitmapFactory.decodeByteArray(data,0,data.length);
-                                        plan.setImage(bitmap);
-                                        plan.setTitle(object.getString("title"));
-                                        plan.setDesc(object.getString("description"));
-                                        plan.setTags(object.getString("hashtag"));
-                                        //finally,add to planlist
-                                        planList.add(plan);
-                                        if(planList.size()==objects.size()) {
-                                            mAdapter = new Adapter(DiscoverActivity.this, planList);
-                                            listOfPlans.setAdapter(mAdapter);
-                                        }
-                                    }else{
-                                        Log.i("Get image","failed");
-                                        e.printStackTrace();
-                                    }
-                                }
-                            });
-                        }
-                    }
-                }else{
-                    e.printStackTrace();
-                }
-            }
-        });
-
-
+		Plan temp = new Plan();
+		planList.add(temp);
+        mAdapter = new Adapter(DiscoverActivity.this,planList);
+        listOfPlans.setAdapter(mAdapter);
+        new MultiplyTask().execute();
 
 	}
 
@@ -106,54 +56,56 @@ public class DiscoverActivity extends AppCompatActivity {
 		BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigation);
 		BottomNavigationViewHelper.enableBottomNavigation(DiscoverActivity.this,bottomNavigation);
 	}
-	public class MultiplyTask extends AsyncTask<Void,Void,Void>
-	{
 
+    private class MultiplyTask extends AsyncTask<Void,Void,Void>
+	{
 		@Override
 		protected Void doInBackground(Void... voids) {
-		    /*planList.clear();
-            ParseQuery<ParseObject> query = ParseQuery.getQuery("Plan");
-            query.whereEqualTo("parent", ParseUser.getCurrentUser());
+            planList.clear();
+            ParseQuery<ParseObject> query = new ParseQuery<>("Plan");
+            final SharedPreferences sharedPreferences = DiscoverActivity.this.getSharedPreferences("SharedPref",MODE_PRIVATE);
+            String username = sharedPreferences.getString("USERNAME",null);
+            query.whereEqualTo("userId", username);
+            query.orderByAscending("createdAt");
+            query.setLimit(3);
             query.findInBackground(new FindCallback<ParseObject>() {
                 @Override
-                public void done(List<ParseObject> objects, ParseException e) {
+                public void done(final List<ParseObject> objects, ParseException e) {
                     if(e==null && objects.size()>0){
-                        for(ParseObject object:objects){
+                        for(final ParseObject object:objects){
                             final Plan plan = new Plan();
-                            plan.setTitle(object.getString("title"));
-                            plan.setDesc(object.getString("description"));
-                            plan.setTags(object.getString("hashtag"));
-
                             //load image to view
                             ParseFile image = (ParseFile) object.get("image");
-
                             if(image!=null){
                                 image.getDataInBackground(new GetDataCallback() {
                                     @Override
                                     public void done(byte[] data, ParseException e) {
                                         if(e==null&&data!=null){
+                                            Log.i("Get image","successful");
                                             Bitmap bitmap = BitmapFactory.decodeByteArray(data,0,data.length);
                                             plan.setImage(bitmap);
+                                            plan.setTitle(object.getString("title"));
+                                            plan.setDesc(object.getString("description"));
+                                            plan.setTags(object.getString("hashtag"));
+                                            //finally,add to planlist
+                                            planList.add(plan);
+                                            if(planList.size()==3){
+                                                mAdapter.notifyDataSetChanged();
+                                            }
+                                        }else{
+                                            Log.i("Get image","failed");
+                                            e.printStackTrace();
                                         }
                                     }
                                 });
                             }
-
-                            //finally,add to planlist
-                            planList.add(plan);
                         }
+                    }else{
+                        e.printStackTrace();
                     }
                 }
-            });*/
-			return null;
+            });
+            return null;
 		}
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-		    /*if(planList.size()>0) {
-                mAdapter = new Adapter(DiscoverActivity.this, planList);
-                listOfPlans.setAdapter(mAdapter);
-            }*/
-        }
     }
 }
